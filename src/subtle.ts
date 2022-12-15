6fb72e524b74c566fc427b14d3b05c0e6beb2dfb
Alexandra Baranova <alexandra.baranova@bullish.com>
2022-12-15 13:31:46 +0800

    feat: support crypto in vite

--- src/subtle.ts
+++ src/subtle.ts
@@ -1,5 +1,3 @@
-import { webcrypto } from 'crypto'
-
 const isBrowser = typeof window !== 'undefined'
-
-export const subtle = isBrowser ? window.crypto.subtle : webcrypto.subtle
+// eslint-disable-next-line @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-var-requires
+export const subtle = isBrowser ? window.crypto.subtle : (require('crypto').webcrypto.subtle as SubtleCrypto)
