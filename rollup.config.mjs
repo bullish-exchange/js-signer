d76bbde667b9410d0ad7fe1f8aaffafa006190c4
Dominic <dominic.tobias@bullish.com>
2024-12-12 17:19:17 +0800

    fix(BULL-44449): fix dts overwriting cjs and keep cjs for react-native metro bundler

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -7,30 +7,41 @@ import tsconfig from './tsconfig.json' with { type: 'json' }
 
 const isProd = process.env.NODE_ENV === 'production'
 
-export default {
-  plugins: [
-    isProd && cleaner({ targets: [tsconfig.compilerOptions.outDir] }),
-    esbuild({
-      target: 'es2023',
-      define: {
-        __VERSION__: '"x.y.z"',
-      },
-      loaders: {
-        '.json': 'json',
-      },
-    }),
-    dts(),
-    !isProd &&
-      visualizer({
-        emitFile: true,
-        file: 'stats.html',
-        template: 'treemap',
+export default [
+  {
+    plugins: [
+      isProd && cleaner({ targets: [tsconfig.compilerOptions.outDir] }),
+      esbuild({
+        target: 'es2023',
+        define: {
+          __VERSION__: '"x.y.z"',
+        },
+        loaders: {
+          '.json': 'json',
+        },
       }),
-  ],
-  input: 'src/index.ts',
-  output: {
-    file: 'dist/index.js',
-    format: 'es',
-    sourcemap: true,
+      !isProd &&
+        visualizer({
+          emitFile: true,
+          file: 'stats.html',
+          template: 'treemap',
+        }),
+    ],
+    input: 'src/index.ts',
+    output: [
+      {
+        file: 'dist/index.cjs.js',
+        format: 'cjs',
+      },
+      {
+        file: 'dist/index.js',
+        format: 'es',
+      },
+    ],
   },
-}
+  {
+    input: './src/index.ts',
+    output: [{ file: 'dist/index.d.ts', format: 'es' }],
+    plugins: [dts()],
+  },
+]
