4d4944de86337c9577535d0d6bad104b592a731a
Dominic <dominic.tobias@bullish.com>
2024-12-12 17:51:20 +0800

    fix(BULL-44449): tweak subtle import

--- src/subtle.ts
+++ src/subtle.ts
@@ -1,2 +1,4 @@
-// eslint-disable-next-line @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-var-requires, @typescript-eslint/no-require-imports
-export const subtle = window.crypto?.subtle || (require('crypto').subtle as SubtleCrypto)
+// window exists in envs like react-native so we also check for the presense of `subtle`. We can alias the "crypto" module in react-native with react-native-quick-crypto.
+export const subtle =
+  // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-var-requires, @typescript-eslint/no-require-imports
+  (typeof window !== 'undefined' && window.crypto?.subtle) || (require('crypto').subtle as SubtleCrypto)
